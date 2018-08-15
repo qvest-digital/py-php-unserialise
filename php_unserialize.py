0022d463054fe2a5dc98ddc800b0eb5f3d944bef
mirabilos <t.glaser@tarent.de>
2018-08-15 17:30:00 +0200

    Python3 bytes vs. unicode…

--- php_unserialize.py
+++ php_unserialize.py
@@ -30,6 +30,13 @@
 def unserialize(s):
     return _unserialize_var(s)[0]
 
+if type('a') == type(u'a'):
+    def _unserialize_tostr(b):
+        return b.decode(errors='surrogateescape')
+else:
+    def _unserialize_tostr(b):
+        return b
+
 def _unserialize_var(s):
     return (
         { 'i' : _unserialize_int
@@ -42,29 +49,29 @@ def _unserialize_var(s):
         , 'C' : _unserialize_custom
         , 'R' : _unserialize_reference_value
         , 'r' : _unserialize_reference_object
-        }[s[0]](s[2:]))
+        }[_unserialize_tostr(s[0:1])](s[2:]))
 
 def _unserialize_int(s):
-    x = s.partition(';')
-    return (int(x[0]), x[2])
+    x = s.partition(';'.encode())
+    return (int(_unserialize_tostr(x[0])), x[2])
 
 def _unserialize_bool(s):
-    x = s.partition(';')
-    return (x[0] == '1', x[2])
+    x = s.partition(';'.encode())
+    return (_unserialize_tostr(x[0]) == '1', x[2])
 
 def _unserialize_double(s):
-    x = s.partition(';')
-    return (float(x[0]), x[2])
+    x = s.partition(';'.encode())
+    return (float(_unserialize_tostr(x[0])), x[2])
 
 def _unserialize_null(s):
     return (None, s)
 
 def _unserialize_string(s):
-    (l, _, s) = s.partition(':')
-    return (s[1:int(l)+1], s[int(l)+3:])
+    (l, _, s) = s.partition(':'.encode())
+    return (_unserialize_tostr(s[1:int(l)+1]), s[int(l)+3:])
 
 def _unserialize_array(s):
-    (l, _, s) = s.partition(':')
+    (l, _, s) = s.partition(':'.encode())
     a, k, s = {}, None, s[1:]
 
     for i in range(0, int(l) * 2):
@@ -79,13 +86,13 @@ def _unserialize_array(s):
     return (a, s[1:])
 
 def _unserialize_custom(s):
-    (l, _, s) = s.partition(':')
-    (n, _, s) = s.partition(':')
-    (l, _, s) = s.partition(':')
+    (l, _, s) = s.partition(':'.encode())
+    (n, _, s) = s.partition(':'.encode())
+    (l, _, s) = s.partition(':'.encode())
 
     l = int(l)
     d = s[1:l+1]
-    aa = { 'custom': n[1:-1], 'data': d }
+    aa = { 'custom': _unserialize_tostr(n[1:-1]), 'data': _unserialize_tostr(d) }
 
     try:
         aa['parsed'] = unserialize(d)
@@ -95,9 +102,9 @@ def _unserialize_custom(s):
     return (aa, s[l+2:])
 
 def _unserialize_object(s):
-    (l, _, s) = s.partition(':')
-    (n, _, s) = s.partition(':')
-    (l, _, s) = s.partition(':')
+    (l, _, s) = s.partition(':'.encode())
+    (n, _, s) = s.partition(':'.encode())
+    (l, _, s) = s.partition(':'.encode())
     a, k, s = {}, None, s[1:]
 
     for i in range(0, int(l) * 2):
@@ -109,14 +116,14 @@ def _unserialize_object(s):
         else:
             k = v
 
-    aa = { 'class': n[1:-1], 'data': a }
+    aa = { 'class': _unserialize_tostr(n[1:-1]), 'data': a }
     return (aa, s[1:])
 
 # we should probably decode that, but JSON doesn’t support it anyway
 # cf. http://www.phpinternalsbook.com/classes_objects/serialization.html
 def _unserialize_reference(what, s):
-    x = s.partition(';')
-    return (['reference', what, int(x[0])], x[2])
+    x = s.partition(';'.encode())
+    return (['reference', what, int(_unserialize_tostr(x[0]))], x[2])
 
 def _unserialize_reference_value(s):
     return _unserialize_reference('value', s)
@@ -130,7 +137,12 @@ if __name__ == "__main__":
     import sys
     import json
 
-    serialised = sys.stdin.read()
+    if sys.version_info >= (3, 0):
+        stdin = sys.stdin.buffer
+    else:
+        stdin = sys.stdin
+
+    serialised = stdin.read()
     unserialised = unserialize(serialised)
     # we need sort_keys because they are internally unordered anyway
     print(json.dumps(unserialised, ensure_ascii=False, indent=True, sort_keys=True))
